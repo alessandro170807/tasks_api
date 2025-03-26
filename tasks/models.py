e1d5dccfe669920ecd2144c82668d43efb52f14e
Carlos Alessandro <alessandromota1714@gmail.com>
2025-03-26 09:46:09 -0300

    copia repositorio do professor

--- tasks/models.py
+++ tasks/models.py
@@ -7,7 +7,7 @@ class Task(models.Model):
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
     finished_at = models.DateTimeField(blank=True, null=True)
-    user =models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def is_completed(self):
         return self.finished_at is not None
@@ -15,3 +15,15 @@ class Task(models.Model):
     def __str__(self):
         return self.title
     
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField(
+        upload_to='profile_images/',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.user.username
